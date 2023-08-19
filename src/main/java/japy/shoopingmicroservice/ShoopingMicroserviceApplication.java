c2a018bdb3855b92d01a4ef051195960b945ba3f
jafet <jafetpintle@gmail.com>
2023-08-19 16:22:03 -0600

    initial commit

---
+++ src/main/java/japy/shoopingmicroservice/ShoopingMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package japy.shoopingmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoopingMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoopingMicroserviceApplication.class, args);
+	}
+
+}
